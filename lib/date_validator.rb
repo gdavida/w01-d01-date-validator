a9b2537fb40c309aa7242b3ec127af472dd5ef92
Davida Gaffney <gdavida@gmail.com>
2016-03-14 21:59:16 -0500

    simplified code

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -15,51 +15,27 @@
 # This method should, in its final form, not do any output.
 
 def valid_date?(month, day, year)
- m_valid = false
- d_valid = false
- y_valid = false
- leap_year_valid = false
- days_per_month = 0
-
-
-	#testing to see if this is a leap_year, which is every 4th year, so divisible by 4 UNLESS it is divisible by 100 but not 400
-	def leap_year 
-		if year % 4 == 0 && year % 400 == 0
-			leap_year_valid = true
-		elsif year % 4 == 0 && year % 100 != 0
-			leap_year_valid = true
-		else leap_year_valid = false
-		end
-	end
-
-#set the valid number of days per that particular month
-if month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12
-	days_per_month = 31
-elsif  month == 4 || month == 6 || month == 8 || month == 11
-	days_per_month = 30
-elsif month == 2 && leap_year == true
-	days_per_month = 29
-else days_per_month = 28
-
-#check if the month is between 1 and 12, inclusive
-if month >= 1 && month <= 12
-	m_valid = true
-else return false
-end
-
-#check if year is between 1880 and 2280 inclusive
-if year >= 1880 && year <= 2280
-	y_valid = true
-else return false
-end
-
-#check if date is valid
-def check_day(date)
-	if date >= 1 && date <= days_per_month
-		return true
-	else return false
-	end
-end
-	
+    leap_year = nil
+    if year % 4 == 0 && year % 400 == 0
+		leap_year = true
+	elsif year % 4 == 0 && year % 100 != 0
+		leap_year = true
+	else leap_year = false
+    end
+    
+    if day < 1 || day > 31
+        return false
+    elsif month < 1 || month > 12
+        return false
+    elsif year < 1880 || year > 2280
+        return false
+    elsif month == (4 || 6 || 9 || 11) && day > 30
+        return false
+    elsif month == 2 && day > 29
+        return false
+    elsif month == 2 && leap_year == false && day > 28
+        return false
+    else return true
+    end
 
 end
